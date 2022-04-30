8f5e71947b1c2d916858d4381cf45c33a7013394
anamikarawat <anamikarawart@gmail.com>
2022-04-30 22:22:48 +0530

    add

--- React Projects/movieapp/src/components/Banner.js
+++ React Projects/movieapp/src/components/Banner.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
 
+import { movies } from '../movieData'
+
 export class Banner extends Component {
+
+
     render() {
+        let moviesElem = movies.results[Math.floor((Math.random() * 10))]
+        let backDrop = moviesElem.backdrop_path
         return (
             //app.css mea styling karege with banner-card
-            <div className="card banner-card">
-                <img src="https://th.bing.com/th/id/R.3f47d77cf1341bcb9c688560b14fa72d?rik=425Fd7BguNLMlw&riu=http%3a%2f%2fcafmp.com%2fwp-content%2fuploads%2f2013%2f01%2fBrave-Teaser.jpg&ehk=%2fo0L2xkJcBFjh%2be%2fhxk5STfR%2bsP%2bxbTNVWAQ3JX%2fRHI%3d&risl=&pid=ImgRaw&r=0" className="card-img-top banner-img" alt="..." />
+            <div className="card banner-card" >
+                <img src={`https://image.tmdb.org/t/p/original${backDrop}`} className="card-img-top banner-img" alt="..." />
 
-
-                <h5 className="card-title banner-title">Card title</h5>
-                <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-
+                <h5 className="card-title banner-title">Jurassic Hunt</h5>
+                <p className="card-text  banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
             </div>
         )
--- React Projects/movieapp/src/components/MoviesList.js
+++ React Projects/movieapp/src/components/MoviesList.js
@@ -1,16 +1,117 @@
 import React, { Component } from 'react'
+//https://www.themoviedb.org/documentation/api API Movie Database
 
+import { movies } from '../movieData'
+import axios from 'axios'
 
-import {movies} from '../movieData'
+export class MovieList extends Component {
+  constructor() {
+    console.log('construtor first')
+    super();
 
-export class MoviesList extends Component {
+    this.state = {
+      hover: "",
+      parr: [1],
+      movies: [],
+
+    };
+  }
+  async componentDidMount() {
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=a6e4b08611b7c42ef51d22afd6405f81&language=en-US&page=1`)
+    let movieData = res.data
+    console.log(movieData)
+    this.setState({
+      movies: [...movieData.results]
+    })
+    console.log('mounting done with CDM third')
+  }
   render() {
-    return (
-      <div>
 
-      </div>
-    )
+    console.log('render second')
+
+    ////to get the movies name
+    // console.log(moviesArr)
+    // return (
+    //   <div>
+    //     {
+    //       moviesArr.map((movieElem) => (
+    //         <h1>{movieElem.title}</h1>
+    //       ))
+    //     }
+    //   </div>
+    // )
+    //************************************************ */
+
+    return (
+
+
+      <>
+        <div>
+          <h3 className="text-center">
+            <strong>Trending</strong>
+          </h3>
+        </div>
+
+        <div className="movies-list">
+          {moviesArr.map((movieElem) => (
+            <div
+              className="card movie-card"
+              onMouseEnter={() => this.setState({ hover: movieElem.id })}
+              onMouseLeave={() => this.setState({ hover: "" })}
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/original${movieElem.backdrop_path}`}
+                style={{ height: "40vh", width: "20vw" }}
+                className="card-img-top movie-img"
+                alt="..."
+              />
+
+              <h5 className="card-title movie-title">{movieElem.title}</h5>
+
+              <div
+                className="button-wrapper"
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                {this.state.hover == movieElem.id && (
+                  <a
+                    href="#"
+                    className="btn btn-primary movies-button text-center"
+                  >
+                    Add to Favourites
+                  </a>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <nav aria-label="Page navigation example">
+            <ul className="pagination">
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  Previous
+                </a>
+              </li>
+
+              {this.state.parr.map((value) => (
+                <li className="page-item">
+                  <a className="page-link" href="#">
+                    {value}
+                  </a>
+                </li>
+              ))}
+
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  Next
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </>
+    );
   }
 }
 
-export default MoviesList
+export default MovieList;
