b1ef5671106e7eafee3c779f98b307d391591a4a
anamikarawat <anamikarawart@gmail.com>
2022-05-04 21:49:32 +0530

    add

--- React Projects/movieapp/src/App.js
+++ React Projects/movieapp/src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Banner from './components/Banner';
 import MoviesList from './components/MoviesList';
 import NavBar from './components/NavBar';
-
+import Favourites from './components/Favourites';
 function App() {
   return (
     <>
       <NavBar />
-      <Banner />
-      <MoviesList/>
+      {  /* <Banner />
+      <MoviesList/>*/ }
+      <Favourites />
     </>
 
   );
---
+++ interview/ques.js
@@ -0,0 +1,4 @@
+//https://leetcode.com/problems/max-chunks-to-make-sorted/
+
+
+//https://leetcode.com/playground/NfECNd95
