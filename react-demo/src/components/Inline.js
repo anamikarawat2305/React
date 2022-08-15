f9274bd22aeef0de545b8a7f43f0ec6903176de1
anamikarawat <anamikarawart@gmail.com>
2022-08-15 20:54:00 +0530

    add

--- react-demo/src/components/Inline.js
+++
@@ -1,9 +0,0 @@
-// Inlne styling
-export const Inline = () => {
-  const heading = {
-    fontSize: "72px",
-    color: "blue",
-  };
-  return <h1 style={heading}>Inline</h1>;
-  
-};
--- react-demo/src/components/StyleSheet.js
+++
@@ -1,5 +0,0 @@
-import "./myStyles.css";
-//css style sheet
-export const StyleSheet = () => {
-  return <h1 className="primary">StyleSheet</h1>;
-};
